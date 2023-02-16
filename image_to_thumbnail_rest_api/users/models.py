6abc41a7200dafeded67a5b3ded20c37b7321cc8
ikolokotronis <ioanniskolokotronis1@gmail.com>
2023-02-16 11:46:34 +0100

    user + tier model

--- image_to_thumbnail_rest_api/users/models.py
+++ image_to_thumbnail_rest_api/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User Model
+    """
+
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    tier = models.ForeignKey("Tier", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Tier(models.Model):
+    """
+    This model is used to store the different tiers a user can have.
+    """
+
+    name = models.CharField(max_length=255)
+    thumbnail_height = models.IntegerField(
+        null=True, blank=True
+    )
+    presence_of_original_file_link = models.BooleanField()
+    ability_to_fetch_expiring_link = models.BooleanField()
+
+    def __str__(self):
+        return self.name
